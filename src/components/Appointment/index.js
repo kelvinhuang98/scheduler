685288e8c09c43d8e409a3cf3861ac12b3a8d267
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-12-02 23:47:25 -0500

    Incorporate the Show and Empty components in the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,22 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
-export default function Appointment() {
-  return <article className="appointment"></article>;
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }

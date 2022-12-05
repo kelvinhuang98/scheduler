ed2dae901d8e49e3cbdd5b326c6b5ade4ea9d104
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-12-05 14:53:43 -0500

    Add prop-type test for InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem.js";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const interviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
@@ -23,3 +24,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
